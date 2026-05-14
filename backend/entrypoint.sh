da11d01d0052790831ed56c2841a0db0da089725
Kim T <kimturley@gmail.com>
2026-05-13 22:59:07 -0700

    chore: build entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-mkdir -p "$SCRIPT_DIR/out"
-cp -r "$SCRIPT_DIR/src/sources" "$SCRIPT_DIR/out/"
-cp -r "$SCRIPT_DIR/src/nfc" "$SCRIPT_DIR/out/"
+cd /backend
+
+mkdir -p out
+cp -r src/sources out/
+cp -r src/nfc out/
