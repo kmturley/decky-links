2c2d261de008f67ccdf169bdde9cfd90bf163cd3
Kim T <kimturley@gmail.com>
2026-05-13 22:31:32 -0700

    chore: ensure backend dependencies are built into zip

--- .vscode/build.sh
+++ .vscode/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 CLI_LOCATION="$(pwd)/cli"
 echo "Building plugin in $(pwd)"
+
+echo "Installing Python dependencies into py_modules/..."
+pip install -r requirements.txt --target=./py_modules --upgrade
+
 printf "Please input sudo password to proceed.\n"
 
 # read -s sudopass
--- setup_test_env.sh
+++ setup_test_env.sh
@@ -6,6 +6,7 @@ python3 -m venv .venv
 source .venv/bin/activate
 
 echo "Installing requirements..."
+pip install -r requirements.txt
 pip install -r tests/requirements.txt
 
 echo ""
